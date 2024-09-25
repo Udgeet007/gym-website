2800a27895c8b0f14d1b383f79d6a214e3d3f30c
Udgeet <bhatt8udgeet@gmail.com>
2024-09-26 01:56:58 +0530

    contact component done

--- frontend/src/Components/Contact.jsx
+++ frontend/src/Components/Contact.jsx
@@ -1,7 +1,40 @@
-
+import { Loader } from "lucide-react";
+import { useState } from "react";
+// import {ClipLoader} from "lucide-react";
 
 export const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="contact">
+      <form>
+        <h1>Contack us</h1>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Message</label>
+          <input type="text" 
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}/>
+        </div>
+        <button type="submit" disabled={loading} style={{display: "flex", justifyContent:"center", alignItems:"center", gap:"15px"}}>{loading && <Loader size={20} color="white"/>}Send Message</button>
+      </form>
+    </section>
+  );
+};
